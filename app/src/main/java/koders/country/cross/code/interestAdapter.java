052813c278708344c9fadec770667e6d8f75e70b
lkroeker <sparhawk@overclocked.net>
2015-02-21 16:53:01 -0500

    +Added interestAdapter.java +Added interest_row.xml +Updated styles/theme to make things look pretty...er

--- app/src/main/java/koders/country/cross/code/interestAdapter.java
+++ app/src/main/java/koders/country/cross/code/interestAdapter.java
@@ -6,19 +6,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
 import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.ArrayList;
 import koders.country.cross.code.dataapi.ConcreteDataProvider;
 import koders.country.cross.code.dataapi.DataProvider;
 
-class customAdapter extends ArrayAdapter<String> {
+class interestAdapter extends ArrayAdapter<String> {
 
-    int picsSt[] = {R.drawable.cube,R.drawable.cube,R.drawable.cube,R.drawable.cube,R.drawable.cube,R.drawable.cube,R.drawable.cube,R.drawable.cube,R.drawable.cube,R.drawable.cube,R.drawable.cube};
     int list;
 
-    customAdapter(Context context, int item, ArrayList<String> occupations) {
-        super(context, R.layout.custom_row, occupations);
+    interestAdapter(Context context, int item, ArrayList<String> occupations) {
+        super(context, R.layout.interest_row, occupations);
         list = item;
         //picsSt = pics;
     }
@@ -26,17 +26,14 @@ class customAdapter extends ArrayAdapter<String> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater lukesInflater = LayoutInflater.from(getContext());
-        View customView = lukesInflater.inflate(R.layout.custom_row, parent, false);
+        View customView = lukesInflater.inflate(R.layout.interest_row, parent, false);
 
         String singleOccupation = getItem(position);
 
-        TextView lukesText = (TextView) customView.findViewById(R.id.arrayText);
-        ImageView lukesImage = (ImageView) customView.findViewById(R.id.arrayImage);
+        CheckedTextView lukesText = (CheckedTextView) customView.findViewById(R.id.checkedTextView);
 
         lukesText.setText(singleOccupation);
         lukesText.setTextColor(Color.LTGRAY);
-
-        lukesImage.setImageResource(picsSt[position]);
 
         return customView;
     }
